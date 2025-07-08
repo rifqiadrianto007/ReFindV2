4cec02065c6ed193919cae3b8e01b721ce844026
rifqiadrianto007 <232410103042@mail.unej.ac.id>
2025-07-08 16:05:10 +0700

    Add favicon link to login page for improved branding

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>ReFind.</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('img/logo 1.png') }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
     <link href="https://fonts.cdnfonts.com/css/nexa" rel="stylesheet" />
